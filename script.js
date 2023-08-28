c49cf76a48a1dd77210be4c27fb5896b902fc777
Neung <122659280+jcneung@users.noreply.github.com>
2023-08-28 22:45:13 +0700

    first draft

---
+++ script.js
@@ -0,0 +1,44 @@
+// Start 
+const startPhrases = [
+   "This week,",
+   "Today,",
+   "In the coming days,",
+   "Soon,",
+   "The stars suggest that",
+   "The universe reveals that",
+   "Your energies point towards"
+];
+
+// Middle
+const eventPhrases = [
+   "a mysterious encounter",
+   "an unexpected change",
+   "a stroke of luck",
+   "a challenge",
+   "a surprise",
+   "a moment of clarity",
+   "an exciting proposition",
+   "a fortuitous meeting"
+];
+
+// End
+const guidancePhrases = [
+   "will reshape your perspective.",
+   "brings new opportunities.",
+   "will test your patience.",
+   "will enlighten your spirit.",
+   "ushers in joy and happiness.",
+   "needs your full attention.",
+   "will introduce you to new experiences.",
+   "can be a game-changer."
+];
+
+function genMessage() {
+   const s = Math.floor(Math.random() * startPhrases.length);
+   const m = Math.floor(Math.random() * eventPhrases.length);
+   const e = Math.floor(Math.random() * guidancePhrases.length);
+
+   return startPhrases[s] + ' ' + eventPhrases[m] + ' ' + guidancePhrases[e];
+}
+
+console.log(genMessage());
